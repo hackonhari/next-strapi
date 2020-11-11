0b288ac19cf2cfb663d49c2e722def24fc4a38b5
hackon hari <hackonhari@gmail.com>
2020-11-11 12:22:57 +0530

    next config image

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      images: {
+        domains: ["localhost"],
+      },
+    };
+  }
+
+  return {
+    images: {
+      domains: ["res.cloudinary.com"],
+    },
+  };
+};
