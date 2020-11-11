6fad864a06afef4b1d50d52b9f6c298b42986d69
hackon hari <hackonhari@gmail.com>
2020-11-11 12:39:02 +0530

    next config author

--- components/avatar.js
+++ components/avatar.js
@@ -1,15 +1,24 @@
+import Image from "next/image";
 export default function Avatar({ name, picture }) {
+  console.log(picture[0]);
   const url = picture.url ?? picture[0].url;
+  const width = picture.width ?? picture[0].width;
+  const height = picture.height ?? picture[0].height;
 
+  const imageUrl = `${
+    url.startsWith("/") ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ""
+  }${url}`;
   return (
-    <div className="flex items-center">
-      <img
-        src={`${
-          url.startsWith("/") ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ""
-        }${url}`}
-        className="w-12 h-12 rounded-full mr-4 grayscale"
-        alt={name}
-      />
+    <div className="flex items-center ">
+      <div className="w-16 rounded-full mr-4 grayscale">
+        <Image
+          className=" rounded-full mr-4 grayscale"
+          src={imageUrl}
+          alt={name}
+          width={width}
+          height={height}
+        />
+      </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
